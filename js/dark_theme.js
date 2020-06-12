8bdc9ee360f60ec65d068cecae46de7b2c77a37a
ORUC QAFAROV <orr888@gmail.com>
2020-06-12 14:55:33 +0400

    a

---
+++ js/dark_theme.js
@@ -0,0 +1,20 @@
+//  Скрипт для переключателя страницы на чёрную тему
+
+var checkbox = document.querySelector('.theme-switch__checkbox');
+
+checkbox.addEventListener('change', function () {
+    transition();
+    if (this.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light');
+    }
+})
+
+// Функция замедлении времени переключателя темы
+function transition() {
+    document.documentElement.classList.add('transition');
+    setTimeout(function () {
+        document.documentElement.classList.remove('transition');
+    }, 250)
+}
