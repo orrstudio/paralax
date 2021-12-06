f3ab38715569c52cca1f53b43c262dbbefca770d
Oruc Qafarov <orr888@gmail.com>
2021-12-06 17:13:39 +0400

    Update to lack team

--- js/dark_theme.js
+++ js/dark_theme.js
@@ -5,9 +5,9 @@ var checkbox = document.querySelector('.theme-switch__checkbox');
 checkbox.addEventListener('change', function () {
     transition();
     if (this.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-    } else {
         document.documentElement.setAttribute('data-theme', 'light');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'dark');
     }
 })
 
