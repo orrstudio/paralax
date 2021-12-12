4ee017a1fc86bba47f06b2ba7569826cb1347da2
Oruc Qafarov <orr888@gmail.com>
2021-12-12 22:34:40 +0400

    a

--- js/js.js
+++ js/js.js
@@ -1,24 +1,3 @@
-//  Скрипт для переключателя страницы на чёрную тему
-
-var checkbox = document.querySelector('.theme-switch__checkbox');
-
-checkbox.addEventListener('change', function () {
-    transition();
-    if (this.checked) {
-        document.documentElement.setAttribute('data-theme', 'light');
-    } else {
-        document.documentElement.setAttribute('data-theme', 'dark');
-    }
-})
-
-// Функция замедлении времени переключателя темы
-function transition() {
-    document.documentElement.classList.add('transition');
-    setTimeout(function () {
-        document.documentElement.classList.remove('transition');
-    }, 250)
-}
-
 // Переключение между добавлением и удалением «responsive» класса в topnav, когда пользователь нажимает на значок
 function myFunction() {
     var x = document.getElementById("myTopnav");
@@ -31,14 +10,6 @@ function myFunction() {
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () { scrollFunction() };
-
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("myBtn").style.display = "block";
-    } else {
-        document.getElementById("myBtn").style.display = "none";
-    }
-}
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
---
+++ js/scrollFunction.js
@@ -0,0 +1,7 @@
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
