b2edd4828bc2c9f19c114d28178fc55920753fcb
Oruc Qafarov <orr888@gmail.com>
2021-12-12 22:29:42 +0400

    Vostanovleniye js

---
+++ js/js.js
@@ -0,0 +1,47 @@
+//  Скрипт для переключателя страницы на чёрную тему
+
+var checkbox = document.querySelector('.theme-switch__checkbox');
+
+checkbox.addEventListener('change', function () {
+    transition();
+    if (this.checked) {
+        document.documentElement.setAttribute('data-theme', 'light');
+    } else {
+        document.documentElement.setAttribute('data-theme', 'dark');
+    }
+})
+
+// Функция замедлении времени переключателя темы
+function transition() {
+    document.documentElement.classList.add('transition');
+    setTimeout(function () {
+        document.documentElement.classList.remove('transition');
+    }, 250)
+}
+
+// Переключение между добавлением и удалением «responsive» класса в topnav, когда пользователь нажимает на значок
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
